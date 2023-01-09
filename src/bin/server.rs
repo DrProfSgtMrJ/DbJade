39256cbbcc4a22ffded2abee4b983068ee3bd8a2
DrProfSgtMrJ <jadedm@Jades-MacBook-Pro.local>
2023-01-09 10:41:50 -0800

    fixed send and receive

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,7 +2,7 @@ use dbjade::serverops::ServerOp;
 use tokio::io::{BufReader};
 use tokio::net::TcpStream;
 use tokio::{net::TcpListener, io::AsyncReadExt};
-use tokio::sync::broadcast;
+use tokio::sync::mpsc;
 
 const ADDR: &str = "localhost:7676"; // Your own address : TODO change to be configured
 const CHANNEL_NUM: usize = 100;
@@ -15,30 +15,27 @@ use bincode;
 async fn main() {
      // Create listener instance that bounds to certain address
     let listener = TcpListener::bind(ADDR).await.map_err(|err|  panic!("Failed to bind: {err}")).unwrap();
-    let (tx, mut rx) = broadcast::channel::<(ServerOp, SocketAddr)>(CHANNEL_NUM);
+    let (tx, mut rx) = mpsc::channel::<(ServerOp, SocketAddr)>(CHANNEL_NUM);
     
-
-    loop {
-        if let Ok((mut socket, addr)) = listener.accept().await {
-            let tx = tx.clone();
-            let mut rx = tx.subscribe();
-            println!("Receieved stream from: {}", addr);
-            let mut buf = vec![0, 255];
-            tokio::select! {
-                result = socket.read(&mut buf) => {
-                    match result {
-                        Ok(res) => println!("Bytes Read: {res}"),
-                        Err(_) => println!(""),
-                    }
-                    tx.send((ServerOp::Dummy, addr)).unwrap();
-                }
-                result = rx.recv() =>{
-                    match result {
-                        Ok(msg, ..) => println!("Hit: {:#?}", msg),
-                        Err(err) => eprintln!("Error {err}"),
-                    }
+    tokio::spawn(async move {
+        loop {
+            if let Ok((mut socket, addr)) = listener.accept().await {
+                let tx = tx.clone();
+                println!("Receieved stream from: {}", addr);
+                let mut buf = vec![0, 255];
+                if let Ok(result) = tx.send((ServerOp::Dummy, addr)).await {
+                    println!("Sent dummy data");
                 }
             }
+        }
+    });
+
+    while let Some(serverop) = rx.recv().await {
+        match serverop {
+            (ServerOp::Dummy, ..) => println!("Received a Dummy message"),
+            (ServerOp::ConnectTo { .. }, ..) => println!("Received a ConnectTo message"),
+            (ServerOp::ListDbs, ..) => println!("Received a ListDbs message"),
+            (ServerOp::Disconnect, ..) => println!("Received a Disconnect message"),
         }
     }
 }
